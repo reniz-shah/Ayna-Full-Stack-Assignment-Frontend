e7c347d3e7e7f98a1615cbdbbf3bb6e1b8263eec
reniz-shah <reniz2810@gmail.com>
2024-07-30 14:09:37 +0530

    change base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Ayna-Full-Stack-Assignment-Frontend/",
+  base: "https://reniz-shah.github.io/Ayna-Full-Stack-Assignment-Frontend",
   plugins: [react()],
 })
