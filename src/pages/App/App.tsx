ccd67ce5b719bda2281f859836a34494d6ab595b
reniz-shah <reniz2810@gmail.com>
2024-07-30 14:07:29 +0530

    change routes

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, HashRouter } from "react-router-dom";
 import { getToken } from "../../auth/helper";
 import Chat from "../Chat/Chat";
 import Login from "../Login/Login";
@@ -8,12 +8,14 @@ function App() {
 
   return (
     <>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Login />} />
-        <Route path="/chat" element={getToken() ? <Chat /> : <Navigate to="/login" />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
+      <HashRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/chat" element={getToken() ? <Chat /> : <Navigate to="/login" />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </HashRouter>
     </>
   )
 }
